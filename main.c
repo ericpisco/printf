5f591db73f699931dd603220cb9666e3ed36859c
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-01-23 04:36:03 -0800

    Main files added

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int len;
+
+	char *str = pisco";
+
+	len = _printf
+		("The number A : % d B : % i C : % d D : % i \n" , 'a' , 'b', "pisco" , str);
+
+	printf("length : % d\n" , len);
+
+	return (0);
+
+}
